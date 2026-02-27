1e7a4642f8e9f86a92da274b64c3eab666322062
siva <korasivaliladharreddy@gmail.com>
2026-02-27 16:06:02 +0530

    JS Variables,Datatypes and operators done

---
+++ java script/script.js
@@ -0,0 +1,69 @@
+//  console.log("Hello World");
+// //Reinitialize and redeclare
+// var a; //Global scope
+// a=10;
+// var a;
+// a=20;
+// //Reinitialize
+// let b=10;
+// //let b;
+// b=20;
+// //cant redeclare or reinitialize
+// const c=30; //Block scope
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var num=10;
+// var str="sivalila"; //'' " " 
+// var bool=true;
+// var un;
+// var nu=null;
+// var big=123456789n;
+// var symbol=Symbol('li');
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof un);
+// console.log(typeof nu);
+// console.log(typeof big);
+// console.log(typeof symbol);
+// var arr=[1,2,3,4,5]
+// console.log(typeof arr);
+// var obj={
+//     name:"sivalila",
+//     dept:"DS"
+
+// }
+// console.log(obj);
+// console.log(typeof obj);
+
+//Arithmatic operator
+// var a=10;
+// var b="2"
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a*b);
+// console.log(a**b);
+
+//Relational operator
+// var a=10;
+// var b=20;
+// console.log(a==b);
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+//logical(&&,||,!)
+// var a=true;
+// var b=false;
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+
+//
